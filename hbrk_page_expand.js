928f3c393eb16d79deefe98de7ae809d7177285c
typeg <typeg@users.noreply.github.com>
2016-07-01 02:01:55 +0900

    show all attached images also

--- hbrk_page_expand.js
+++ hbrk_page_expand.js
@@ -1,1 +1,1 @@
-function hbrkPageExpand(){for(var i = 1; i <=tot_pages; i++){$('#contetentArea'+i).hide();}}
+function hbrkPageExpand(){for(var i = 1; i <=tot_pages; i++){$('#contetentArea'+i).hide();}$("div[id^='content_image']").show();}
